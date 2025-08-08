ae9416b88bd8bfdf640d89f2d9332e1aa0bdc8e6
Retty888 <adminische@gmail.com>
2025-08-08 23:30:46 +0300

    Declare config utilities

--- include/config.h
+++ include/config.h
@@ -1,3 +1,13 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 // Configuration file, can be expanded later
+
+namespace Config {
+
+std::vector<std::string> load_selected_pairs(const std::string& filename);
+void save_selected_pairs(const std::string& filename, const std::vector<std::string>& pairs);
+
+} // namespace Config
