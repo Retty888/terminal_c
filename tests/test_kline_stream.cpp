fb1ee700a75634f3d124a7e5f28636c0572e1fc1
Retty888 <adminische@gmail.com>
2025-08-17 22:55:54 +0300

    Add WebSocket close callback and condition-based stream shutdown

--- tests/test_kline_stream.cpp
+++ tests/test_kline_stream.cpp
@@ -15,20 +15,23 @@ public:
   void setUrl(const std::string &) override {}
   void setOnMessage(MessageCallback) override {}
   void setOnError(ErrorCallback cb) override { err_cb_ = std::move(cb); }
+  void setOnClose(CloseCallback cb) override { close_cb_ = std::move(cb); }
   void start() override {
     starts_++;
-    if (err_cb_) {
-      std::thread([cb = err_cb_]() {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        cb();
-      }).detach();
-    }
+    std::thread([ec = err_cb_, cc = close_cb_]() {
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+      if (ec) ec();
+      if (cc) cc();
+    }).detach();
   }
-  void stop() override {}
+  void stop() override {
+    if (close_cb_) close_cb_();
+  }
 
 private:
   int &starts_;
   ErrorCallback err_cb_;
+  CloseCallback close_cb_;
 };
 
 TEST(KlineStreamTest, ReconnectsWithBackoff) {
@@ -55,5 +58,40 @@ TEST(KlineStreamTest, ReconnectsWithBackoff) {
     EXPECT_EQ(delays[0], 1);
     EXPECT_EQ(delays[1], 2);
   }
+}
+
+class BlockingWebSocket : public IWebSocket {
+public:
+  void setUrl(const std::string &) override {}
+  void setOnMessage(MessageCallback) override {}
+  void setOnError(ErrorCallback) override {}
+  void setOnClose(CloseCallback cb) override { close_cb_ = std::move(cb); }
+  void start() override {}
+  void stop() override {
+    if (close_cb_) close_cb_();
+  }
+
+private:
+  CloseCallback close_cb_;
+};
+
+TEST(KlineStreamTest, StopsPromptly) {
+  std::filesystem::path tmp = std::filesystem::temp_directory_path() / "kline_stream_test2";
+  CandleManager mgr(tmp);
+  auto factory = []() { return std::make_unique<BlockingWebSocket>(); };
+  std::atomic<int> sleeps{0};
+  auto sleep_fn = [&](std::chrono::milliseconds d) {
+    ++sleeps;
+    std::this_thread::sleep_for(d);
+  };
+
+  KlineStream ks("btcusdt", "1m", mgr, factory, sleep_fn, std::chrono::milliseconds(1));
+  ks.start(nullptr, nullptr);
+  std::this_thread::sleep_for(std::chrono::milliseconds(5));
+  auto t0 = std::chrono::steady_clock::now();
+  ks.stop();
+  auto elapsed = std::chrono::steady_clock::now() - t0;
+  EXPECT_LT(std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count(), 20);
+  EXPECT_EQ(sleeps.load(), 0);
 }
 
