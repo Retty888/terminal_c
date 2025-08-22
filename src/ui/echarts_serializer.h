373ba421ff34dabe2395c639b73eee5397493823
Retty888 <adminische@gmail.com>
2025-08-22 15:23:40 +0300

    Add Lightweight Charts candle serialization

--- src/ui/echarts_serializer.h
+++ src/ui/echarts_serializer.h
@@ -8,3 +8,7 @@
 // matching ECharts candlestick expectations.
 nlohmann::json SerializeCandles(const std::vector<Core::Candle>& candles);
 
+// Serialize candle data for TradingView Lightweight Charts. Produces an array
+// of objects: { time: <unix-sec>, open, high, low, close }.
+nlohmann::json SerializeCandlesTV(const std::vector<Core::Candle>& candles);
+
