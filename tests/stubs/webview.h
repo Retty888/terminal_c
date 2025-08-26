80a17f49f0f11d895756d7904974ef6c492d96c9
Retty888 <adminische@gmail.com>
2025-08-26 14:18:29 +0300

    Update webview test stub to new callback signature

--- tests/stubs/webview.h
+++ tests/stubs/webview.h
@@ -6,6 +6,6 @@ inline int webview_run(webview_t) { return 0; }
 inline int webview_terminate(webview_t) { return 0; }
 inline int webview_dispatch(webview_t, void (*)(webview_t, void*), void*) { return 0; }
 inline int webview_navigate(webview_t, const char*) { return 0; }
-inline int webview_bind(webview_t, const char*, void (*)(webview_t, const char*, const char*, void*), void*) { return 0; }
+inline int webview_bind(webview_t, const char*, void (*)(const char*, const char*, void*), void*) { return 0; }
 inline int webview_return(webview_t, const char*, int, const char*) { return 0; }
 inline int webview_eval(webview_t, const char*) { return 0; }
